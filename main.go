3eff6a4faf647b674d9a5d1e1fb011edff029e08
vyas-git <vyasreddy.tech@gmail.com>
2021-01-06 19:35:10 +0530

    flappy gopher initial commit

---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/ttf"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(2)
+	}
+}
+func run() error {
+
+	// Init SDL
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		return fmt.Errorf("could not initialize SDL: %v", err)
+	}
+	defer sdl.Quit()
+
+	// Init TTF fonts
+	if err := ttf.Init(); err != nil {
+		return fmt.Errorf("could not initialize TTF: %v", err)
+	}
+	defer ttf.Quit()
+
+	// Create window and renderer
+	w, r, err := sdl.CreateWindowAndRenderer(800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		return fmt.Errorf("could not create window: %v", err)
+	}
+	defer w.Destroy()
+
+	sdl.PumpEvents()
+	// Draw Title Flappy Gopher
+	if err := drawTitle(r); err != nil {
+		return fmt.Errorf("Could not draw title: %v", err)
+	}
+	time.Sleep(5 * time.Second)
+
+	// Draw Background Image
+	if err := drawBackground(r); err != nil {
+		return fmt.Errorf("Could not draw image: %v", err)
+	}
+	time.Sleep(5 * time.Second)
+
+	return nil
+
+}
+
+func drawTitle(r *sdl.Renderer) error {
+	r.Clear()
+
+	// Open font file
+	fontFile, err := ttf.OpenFont("res/fonts/Flappy.ttf", 50)
+	if err != nil {
+		return fmt.Errorf("Could not open font: %v", err)
+	}
+	defer fontFile.Close()
+
+	// Render font
+	color := sdl.Color{R: 255, G: 100, B: 0, A: 255}
+	surface, err := fontFile.RenderUTF8Solid("Flappy Gopher", color)
+	if err != nil {
+		return fmt.Errorf("Could not render font: %v", err)
+	}
+	defer surface.Free()
+
+	// create texture from surface
+	texture, err := r.CreateTextureFromSurface(surface)
+	if err != nil {
+		return fmt.Errorf("Could not create texture: %v", err)
+	}
+	defer texture.Destroy()
+
+	// copy texture to render
+	if err := r.Copy(texture, nil, nil); err != nil {
+		return fmt.Errorf("Could not copy texture: %v", err)
+	}
+
+	r.Present()
+	return nil
+}
+
+func drawBackground(r *sdl.Renderer) error {
+	r.Clear()
+	// Load image texture
+	imgTexture, err := img.LoadTexture(r, "res/imgs/background.png")
+	if err != nil {
+		return fmt.Errorf("Could not load image texture: %v", err)
+	}
+	defer imgTexture.Destroy()
+
+	if err := r.Copy(imgTexture, nil, nil); err != nil {
+		return fmt.Errorf("Could not copy image texture: %v", err)
+	}
+	r.Present()
+	return nil
+}
